52b734a18c3f8721990aaf6f40b5cda8233f6f93
Srini Maddineni <srini@mdstechinc.com>
2019-07-27 14:46:10 -0400

    Modified test to find specific element

--- e2eTests/common_test.js
+++ e2eTests/common_test.js
@@ -7,7 +7,7 @@ Scenario('navigate to homepage', I => {
   I.see('User 4');
   I.saveScreenshot('frontpageScreenshot.png');
 
-  I.click('.smallText');
+  I.click({xpath: '//*[@id="root"]/div/div/div/div/table/tbody/tr[3]/td[3]'});
   I.saveScreenshot('frontpageScreenshot1.png');
   I.fillField('name','test');
   I.saveScreenshot('frontpageScreenshot2.png');
