199a00bce93d6a3adaf9fb9f500b1af653aba521
Srini Maddineni <srini@mdstechinc.com>
2019-07-27 14:52:40 -0400

    Modified test to find specific element

--- e2eTests/common_test.js
+++ e2eTests/common_test.js
@@ -1,18 +1,31 @@
-Feature('Basic test');
+Feature('Acceptance Test');
 
-Scenario('navigate to homepage', I => {
+Before((I) => {
   I.amOnPage('http://react-app:3000');
-  // pause();
+});
+
+Scenario('users are displayed', I => {
   I.waitForElement('.smallText', 30);
   I.see('User 4');
-  I.saveScreenshot('frontpageScreenshot.png');
+  I.saveScreenshot('usersloaded.png');
+});
 
+Scenario('edit user', I => {
   I.click({xpath: '//*[@id="root"]/div/div/div/div/table/tbody/tr[3]/td[3]'});
-  I.saveScreenshot('frontpageScreenshot1.png');
-  I.fillField('name','test');
-  I.saveScreenshot('frontpageScreenshot2.png');
+  I.fillField('name','edit user');
   I.click('Save changes', '.modal-footer');
-  // locate('tr').withAttr({ key: '1' });
-  I.saveScreenshot('frontpageScreenshot3.png');
-  I.see('test');
+  I.saveScreenshot('edituser.png');
+  I.see('edit user');
+});
+
+Scenario('edit user', I => {
+  I.click({xpath: '//*[@id="root"]/div/div/div/div/table/tbody/tr[2]/td[4]'});
+  I.saveScreenshot('deleteuser.png');
+});
+
+Scenario('add user', I => {
+  I.click('Add User');
+  I.fillField('name','add user');
+  I.click('Save changes', '.modal-footer');
+  I.saveScreenshot('adduser.png');
 });
